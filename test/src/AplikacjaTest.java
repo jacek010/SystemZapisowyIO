539e437dccadc39949ce6681033483baeab1758a
jacob <kubarauk@gmail.com>
2023-01-12 11:59:27 +0100

    added test files

---
+++ test/src/AplikacjaTest.java
@@ -0,0 +1,52 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class AplikacjaTest {
+
+    @org.junit.jupiter.api.Test
+    void dodajKurs() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testDodajKurs() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dodajProwadzacego() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dopiszStudenta() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void usunStudentaZKursu() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void przeniesStudenta() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void zapisStudentaNaKurs() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void zwrocStudenta() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void szukajKursu() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void zwrocKurs() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProwadzacy() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void usunKurs() {
+    }
+}
